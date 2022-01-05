c98e2b242ca93d30970e71bdec3d7dfc4db993f3
Thomas Ward <80788878+thomas-ward-raven@users.noreply.github.com>
2022-01-04 19:34:58 -0700

    Adding chart features

--- weights/weights.py
+++ weights/weights.py
@@ -1,6 +1,9 @@
 import os
 import csv
+import matplotlib.pyplot as plt
+
 from datetime import date
+
 
 
 def recordweights(weight, bmi):
@@ -8,6 +11,7 @@ def recordweights(weight, bmi):
         today = date.today()
         csvwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         csvwriter.writerow([today.isoformat(), weight, bmi])
+        print("Your information has been recorded.")
 
 
 def calculatebmi(weight, height):
@@ -21,6 +25,8 @@ def calculatebmi(weight, height):
 # Initialize the CSV file
 fileexists = os.path.isfile("./weights.csv")
 if fileexists is False:
+    print("Unable to find weight history.")
+    print("Starting new history file.")
     with open("weights.csv", "w") as csvfile:
         csvwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         csvwriter.writerow(['Date', 'Weight (KG)', 'BMI'])
@@ -38,7 +44,6 @@ if fileexists is False:
         recordweights(startingweight, bmi)
         csvwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         csvwriter.writerow([firsttime.isoformat(), name, height, startingweight, bmi])
-        print("Your information has been recorded.")
         print("Run the program again to update your weight and see how you've progressed!")
         exit()
 
@@ -63,3 +68,21 @@ recordweights(latestweight, bmi)
 totalloss = float(startingweight) - float(latestweight)
 
 print(f"You have been recording since {firsttime} and have lost {totalloss} kg!")
+
+with open("weights.csv") as csvfile:
+    weightlist = []
+    datelist = []
+    weightsreader = csv.reader(csvfile)
+    i=0
+    for row in weightsreader:
+        i = i+1
+        weightlist.append(row[1])
+        datelist.append(row[0])
+    datelist.pop(0)
+    weightlist.pop(0)
+    print(datelist)
+    plt.plot(datelist,weightlist)
+    plt.title('test')
+    plt.xlabel('test')
+    plt.ylabel('test')
+    plt.savefig('test.png')
